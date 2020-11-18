31db40b48b2478e5c714b1cd66187fc4bda74f3f
Mystic#9697 <shadow6060@users.noreply.github.com>
2020-11-17 20:47:28 -0500

    Added More Stuffs

--- bot.js
+++ bot.js
@@ -3,6 +3,26 @@ const client = new Discord.Client();
 const Tokenfile = require("./auth.json")
 const config = require ("./auth.json");
 
+client.commands = new Discord.Collection();
+
+//command handler
+
+client.on("message", message => {
+	if (message.author.bot) return;
+	if(message.content.indexOf(config.prefix) !== 0) return
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
+  
+	try {
+	  let commandFile = require(`./commands/${command}.js`);
+  
+	  commandFile.run(client, message, args);
+	} catch (err) {
+	  console.error(err);
+	}
+	});
+  
+  //end of command handler
 
 client.on("ready", () => {
 	console.log("The bot is ready.");
