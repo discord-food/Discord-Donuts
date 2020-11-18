3b5af3e6859fa2e6a6ee9ed4d76781d547b741b4
Mystic#9697 <shadow6060@users.noreply.github.com>
2020-11-17 21:00:46 -0500

    Updated Info

--- commands/invite.js
+++ commands/invite.js
@@ -3,8 +3,8 @@ const Discord = require('discord.js')
 exports.run = async (client, msg, args) => {
     let invite = new Discord.MessageEmbed()
         .addField('Here is our invite page!', [
-            "[**Click here to invite our bot**](https://discord.com/api/oauth2/authorize?client_id=733713255024951388&permissions=314561&scope=bot 'Bot invite')",
-            `[**Click here to join our support server!**](https://discord.gg/edBRFGp 'Discord Server')`
+            "[**Click here to invite our bot**](https://discord.com/oauth2/authorize?client_id=778401540133879809&scope=bot&permissions=0 'Bot invite')",
+            `[**Click here to join our support server!**](https://discord.gg/G96bzbY 'Discord Server')`
         ])
     msg.channel.send(invite);
 }
